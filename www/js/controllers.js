3f076b7923fdfa2afbbd1829b389a51686945205
tomershohet <tomershohet@gmail.com>
2015-04-27 13:20:13 +0300

    Update scrolling with tap

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,11 @@ angular.module('starter.controllers', [])
   $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
 })
 
-.controller("HomeCtrl", function($scope, $ionicSlideBoxDelegate) {
+.controller("HomeCtrl", function($scope, $ionicSlideBoxDelegate, $timeout) {
+    var lastScrollTop = 0;
+    $timeout( function() {
+        $ionicSlideBoxDelegate.enableSlide(false);
+    } ,0);
 
     $scope.photosFeed = [{id: "1", name: "Tomer", profilePic: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfp1/v/t1.0-1/p160x160/10561763_10152659558942682_4962942110060764144_n.jpg?oh=b0589e21488ad3ed57fab186c0fa23b0&oe=55AAD54E&__gda__=1437103065_04d2f22df31633e7f6e3105145f7d50b", shootTime: "3 hours ago",
                          subPhotos: [{updated: "1 hours ago", editedBy:"Yael", profilePic:"https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xtf1/v/t1.0-1/c0.0.160.160/p160x160/1604624_605572142832110_73102778_n.jpg?oh=19cfcc49b76ccbaa57d6f1ecb6eb063a&oe=55A1A0AA&__gda__=1440572028_3c8b6ac315d755986291e28902ea618f", picUrl: "http://pe-images.s3.amazonaws.com/photo-effects/photoshop-sunset/sunset-beach.jpg", title: "סוף מתוק עדיף מהתחלה מתוקה", tags: ["Akko", "Food"], likes: ["Yael", "Sam"], likesCounter: 2},
@@ -16,8 +20,8 @@ angular.module('starter.controllers', [])
                          {id: "4", name: "Ariel", profilePic: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xta1/v/t1.0-1/p160x160/10671430_10203844532726980_2736261165339723692_n.jpg?oh=2f78931a5adb73b09561a371f31f2b45&oe=55ABAD2F&__gda__=1436327504_bb29649f42a75b78ae1679df603f4763", shootTime: "12 hours ago",
                          subPhotos: [{updated: "8 hours ago", editedBy:"Mosden", profilePic:"https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpf1/v/t1.0-1/c1.0.160.160/p160x160/1544424_10203434115747213_8615171785042763955_n.jpg?oh=b8e842dd8e116496607acd98e7664cfc&oe=559ED289&__gda__=1436745289_d4053f88227118a47cd83aea841308bd", picUrl: "http://pe-images.s3.amazonaws.com/photo-effects/cc/vignette-radial-filter/original.jpg", title: "Meet Lola", tags: [], liked: true, likes: ["Lee", "Tomer"], likesCounter: 2},
                                      {updated: "11 hours ago", editedBy:"Yael", profilePic:"https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xtf1/v/t1.0-1/c0.0.160.160/p160x160/1604624_605572142832110_73102778_n.jpg?oh=19cfcc49b76ccbaa57d6f1ecb6eb063a&oe=55A1A0AA&__gda__=1440572028_3c8b6ac315d755986291e28902ea618f", picUrl: "http://pe-images.s3.amazonaws.com/photo-effects/cc/vignette-radial-filter/radial-filter-vignette-effect.jpg", title: "תגידו מזל טוב", tags: [], liked: true, likes: ["Lee", "Tomer"], likesCounter: 2}]}];
-
-    $scope.like = function(subPhoto) {
+    
+    $scope.like2 = function(subPhoto) {
         subPhoto.liked = !subPhoto.liked;
         if (subPhoto.liked)
           subPhoto.likes.unshift("Tomer");
@@ -29,13 +33,26 @@ angular.module('starter.controllers', [])
         }
     };
 
-    $scope.nextSlide = function(itemId) {
-      $ionicSlideBoxDelegate.$getByHandle(itemId).next();
+    $scope.nextSlide = function() {
+        $ionicSlideBoxDelegate.next();
+        $ionicSlideBoxDelegate.enableSlide(true);
+    };
+
+    $scope.updateSlide = function(index) {
+        $ionicSlideBoxDelegate.slide(index, 200);
+        $ionicSlideBoxDelegate.enableSlide(true);
     };
 
     $scope.edit = function(photo) {
       //  launchEditor(photo.id, photo.picUrl);
     };
+
+    $scope.onScrollY = function(scrollTop, scrollLeft) {
+      if (Math.abs(scrollTop - lastScrollTop) > 50)
+        $ionicSlideBoxDelegate.enableSlide(false);
+      lastScrollTop = scrollTop;
+    };
+      
 })
 
 .controller('SearchCtrl', function($scope) {})
--- www/js/services.js
+++ www/js/services.js
@@ -18,7 +18,6 @@ angular.module('starter.services', [])
   }
 }])
 
-
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
 
